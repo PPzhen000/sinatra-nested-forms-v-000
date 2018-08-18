3d3844dac8ae6aae97fad70907669670b31ac063
Ariel Zhen <zhenpingping@live.com>
2018-08-18 05:14:05 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,18 @@
-class Ship
+class Ship 
+
+  attr_reader :name, :type, :booty 
+
+  SHIPS = []
+
+  def initialize(args)
+    @name = args[:name]
+    @type = args[:type]
+    @booty = args[:booty]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
+  
 end
